ed36b3204896466c5dc9fc13317edb6260a2d7fd
kotovvv <kotovvv@gmail.com>
2023-05-31 16:17:07 +0300

    item start

--- src/Blocks.js
+++ src/Blocks.js
@@ -8,11 +8,8 @@ const Blocks = memo(({ inStock, container }) => {
   const products = useRef(JSON.parse(tehnokrat.products)).current
   let all_products = []
   products.forEach(e => {
-    // all_products.concat(e.variations)
     Array.prototype.push.apply(all_products, e.variations)
   });
-
-  console.log(all_products)
 
   return createPortal(all_products.map((product, i) => {
 
--- src/ProductCat.js
+++ src/ProductCat.js
@@ -1,5 +1,73 @@
 const ProductCat = () => {
-  return <h1>Product cart</h1>
+  return <div class="product-item">
+    <div class="product-cont">
+      <span class="pl" style="background-color: rgb(30, 188, 150);">Фізична SIM</span>
+      <div class="part">
+        <span>
+          <img src="/wp-content/themes/tehnokrat/img/Mono.png"
+            alt="monobank pay part">
+            <div class="i">
+              <p>«Покупка частинами» від monobank</p>
+              <span>Для оформлення необхідно:</span>
+              <ul>
+                <li>1. Бути клієнтом monobank</li>
+                <li>2. Мати смартфон з додатком monobank</li>
+                <li>3. Перевірити доступний ліміт на розстрочку</li>
+                <li>4. Мати на карті суму для першого платежу</li>
+              </ul>
+              <a href="https://tehnokrat.ua/ua/shipping-and-payment/">Детальніше</a>
+            </div>
+        </span>
+        <span>
+          <img src="/wp-content/themes/tehnokrat/img/Mono.png"
+            alt="monobank pay part">
+            <div class="i">
+              <p>«Покупка частинами» від monobank</p>
+              <span>Для оформлення необхідно:</span>
+              <ul>
+                <li>1. Бути клієнтом monobank</li>
+                <li>2. Мати смартфон з додатком monobank</li>
+                <li>3. Перевірити доступний ліміт на розстрочку</li>
+                <li>4. Мати на карті суму для першого платежу</li>
+              </ul>
+              <a href="https://tehnokrat.ua/ua/shipping-and-payment/">Детальніше</a>
+            </div>
+        </span>
+      </div>
+      <div class="product-img">
+        <img class="prod-image"
+          src="https://tehnokrat.ua/wp-content/uploads/2021/09/iPhone-13-Blue-1.jpg">
+      </div>
+      <a href="https://tehnokrat.ua/ua/product/apple-iphone-13-128gb-blue-mlmt3-iphone-blue128gb-2/"
+        class="name">Apple iPhone 13 128GB
+        Blue MLMT3</a>
+      <div class="price-buy">
+        <a class="buy" href="javascript:void(0)">
+          Купити
+        </a>
+        <div class="sum">
+          <p>51 408<span> грн</span></p>
+          <span>$1 260</span>
+        </div>
+      </div>
+      <div class="features">
+        <ul>
+          <li>
+            <span>Серія:</span> iPhone 13
+          </li>
+          <li>
+            <span>Серія:</span> iPhone 13
+          </li>
+          <li>
+            <span>Серія:</span> iPhone 13
+          </li>
+          <li>
+            <span>Серія:</span> iPhone 13
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 }
 
 export default ProductCat
